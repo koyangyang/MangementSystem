bf39bdc7759f33ba49a33819ef08db8ae07efd3c
koyangyang <koyangyang@outlook.com>
2020-12-30 10:16:40 +0800

    添加Adminer、Costumer、Good

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# MangementSystem
+# 基于Java+MySQL的购物管理系统
+# 所需环境
+- Java：JDK1.8
+- MySQL：8.0
+- JDBC：mysql-connector-java-8.0.16.jar
+# 开发工具
+- idea
+- sqlyog
+# installation
+```git
+git clone https://github.com/koyangyang/MangementSystem.git
+```
---
+++ src/Tings/Costumer.java
@@ -0,0 +1,22 @@
+package Tings;
+
+public class Costumer {
+    private String bh;
+    private String content;
+
+    public String getBh() {
+        return bh;
+    }
+
+    public void setBh(String bh) {
+        this.bh = bh;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
